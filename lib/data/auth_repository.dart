f07fb24b1ac0a0cf7ae1175f97c652ff6ed75b4a
cvallejo <carlosd.vallejo@gmail.com>
2026-01-13 12:48:29 +0100

    repo changes for user firebase creation

--- lib/data/auth_repository.dart
+++ lib/data/auth_repository.dart
@@ -1,21 +1,34 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AuthRepository {
   // Instancia de Firebase Auth (la herramienta oficial)
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   // Método para saber si hay alguien logueado ahora mismo
   User? get currentUser => _firebaseAuth.currentUser;
 
-  // Método para registrarse (Email y contraseña)
+  // SignUp method
   Future<void> signUp({required String email, required String password}) async {
     try {
-      await _firebaseAuth.createUserWithEmailAndPassword(
+      // User creation with Firebase Auth
+      UserCredential userCredential = await _firebaseAuth.createUserWithEmailAndPassword(
         email: email,
         password: password,
       );
+
+      // After successful sign up, create a user document in Firestore
+      if (userCredential.user != null) {
+        await _firestore.collection('users').doc(userCredential.user!.uid).set({
+          'uid': userCredential.user!.uid,
+          'email': email,
+          'createdAt': Timestamp.now(),
+          'isAdmin': false, // Por seguridad
+          // Aquí podríamos añadir 'userName' si lo pidiéramos en el formulario
+        });
+      }
     } catch (e) {
-      // Si falla (ej: correo ya existe), lanzamos el error para que la UI lo sepa
       throw Exception(e.toString());
     }
   }
