3ab32883ac63e5a768738139ef764b03b5879343
cvallejo <carlosd.vallejo@gmail.com>
2026-01-21 13:51:37 +0100

    fix: minSdk and targetSdk changes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,14 +23,11 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.carlosvallejo.appshine"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode
-        versionName = flutter.versionName
+    applicationId = "com.carlosvallejo.appshine"
+    minSdk = 26        
+    targetSdk = 33     
+    versionCode = flutter.versionCode
+    versionName = flutter.versionName
     }
 
     buildTypes {
